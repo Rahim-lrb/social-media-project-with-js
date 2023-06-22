39c6a675c466c485a92ad1e078b7857ae6da798f
rahim <aberrahimcoder@gmail.com>
2023-06-22 14:07:27 +0200

    pagination

--- js/main.js
+++ js/main.js
@@ -1,55 +1,74 @@
-setUpUi();
 const baseUrl = "https://tarmeezacademy.com/api/v1";
+let currentPage = 1;
+let lastPage = 1;
+
+setUpUi();
 getPosts();
-function getPosts() {
-    axios.get(`${baseUrl}/posts`)
+window.addEventListener("scroll", () => {
+    const endOfPage = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+    if (endOfPage && currentPage < lastPage) {
+        currentPage = currentPage + 1;
+        // getPosts(false, currentPage);
+        console.log("hehe");
+    }
+});
+
+
+function getPosts(reload = true, page = 1) {
+    axios.get(`${baseUrl}/posts?limit=2&page=${page}`)
     .then((response) => {
         const posts = response.data.data;
-        for (post of posts) {
-            document.getElementById("posts").innerHTML += "";
-            let postTitle;
-            if (post.title != null) {
-                postTitle = post.title;
-            }
-            let content = `
-            <div class="card shadow">
-                            <h5 class="card-header">
-                            <img src="${post.author.profile_image}" class="rounded-circle border border-2" style="height: 40px; height: 40px;">
-                            <bold>${post.author.username}</bold>
-                            </h5>
-                            <div class="card-body">
-                            <img src="${post.image}" class="w-100">
-                            <h6 class="mt-1" style="color: rgb(179, 176, 176);">${post.created_at} mins ago</h6>
-                            <h5>${postTitle}</h5>
-                            <p>${post.body}</p>
-                            <hr>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                                </svg>
-                                <span>
-                                    ${post.comments_count} comments
-                                    <span id="post-tags-${post.id}">
-                                    </span>
-                                </span>
-                            </div>
-                            </div>
-                    </div>
-            `
-            document.getElementById("posts").innerHTML += content;
+        lastPage = response.data.meta.last_page;
+        console.log(posts)
+        // if (reload) {
+        //     document.getElementById("posts").innerHTML = "";
+        // } 
+        // for (post of posts) {
+        //     let postTitle;
+        //     if (post.title != null) {
+        //         postTitle = post.title;
+        //     }
+        //     let content = `
+        //     <div class="card shadow">
+        //                     <h5 class="card-header">
+        //                     <img src="${post.author.profile_image}" class="rounded-circle border border-2" style="height: 40px; height: 40px;">
+        //                     <bold>${post.author.username}</bold>
+        //                     </h5>
+        //                     <div class="card-body">
+        //                     <img src="${post.image}" class="w-100">
+        //                     <h6 class="mt-1" style="color: rgb(179, 176, 176);">${post.created_at} mins ago</h6>
+        //                     <h5>${postTitle}</h5>
+        //                     <p>${post.body}</p>
+        //                     <hr>
+        //                     <div>
+        //                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+        //                         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+        //                         </svg>
+        //                         <span>
+        //                             ${post.comments_count} comments
+        //                             <span id="post-tags-${post.id}">
+        //                             </span>
+        //                         </span>
+        //                     </div>
+        //                     </div>
+        //             </div>
+        //     `
+        //     document.getElementById("posts").innerHTML += content;
     
-            document.getElementById(`post-tags-${post.id}`).innerHTML += "";
+        //     document.getElementById(`post-tags-${post.id}`).innerHTML += "";
             
-            for (tag of post.tags)  {
-                console.log(tag.name);
-                let tagsContent = `
-                    <button class="btn btn-sm rounded-5" style="background-color: gray; color: white">${tag.name}</button>
-                `
-                document.getElementById("post-tags").innerHTML += tagsContent;
-            }
-        }
+        //     for (tag of post.tags)  {
+        //         console.log(tag.name);
+        //         let tagsContent = `
+        //             <button class="btn btn-sm rounded-5" style="background-color: gray; color: white">${tag.name}</button>
+        //         `
+        //         document.getElementById("post-tags").innerHTML += tagsContent;
+        //     }
+        // }
     })
 }
+
+
 
 
 
@@ -113,9 +132,19 @@ function setUpUi() {
         addBtn.style.display = "block";
         loginDiv.style.setProperty("display", "none", "important");
         logoutDiv.style.setProperty("display", "block");
+        const user = getCurrentUser();
+        document.getElementById("nav-user").innerHTML = user.username;
+        document.getElementById("nav-image").src = user.profile_imageg;
     }
 }
-
+function getCurrentUser() {
+    let user = null;
+    let storageUser = localStorage.getItem('user')
+    if (storageUser != user) {
+        user = JSON.parse(storageUser);
+    } 
+    return user;
+}
 
 // ! log out
 function logOut() {
@@ -131,15 +160,26 @@ function registerBtnClicked() {
     let name = document.getElementById("register-name-input").value;
     let username = document.getElementById("register-username-input").value;
     let password = document.getElementById("register-password-input").value;
+    let image = document.getElementById("register-image-input").files[0];
+
+    console.log(name, username, password, image);
     
-    let params = {
-        "name": name,
-        "username": username,
-        "password": password
-    };
-    console.log(params);
-    axios.post(`${baseUrl}/register`, params)
+    let formData = new FormData();
+    formData.append("name", name);
+    formData.append("username", username);
+    formData.append("password", password);
+    formData.append("image", image);
+
+    let headers = {
+        "Content-Type": "multipart/form-data",
+    }
+
+    axios.post(`${baseUrl}/register`, formData, {
+        headers: headers
+    })
+
     .then((response) => {
+        console.log(response);
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("user", JSON.stringify(response.data.user));
         const modal = document.getElementById("register-modal");
